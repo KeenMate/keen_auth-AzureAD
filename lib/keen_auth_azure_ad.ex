806ea910d6106733a1db15fa933fc9c91c9d009f
Filip Jakab <JakabFilip.FJ@gmail.com>
2021-02-13 00:32:35 +0100

    Added empty mix project

---
+++ lib/keen_auth_azure_ad.ex
@@ -0,0 +1,18 @@
+defmodule KeenAuthAzureAD do
+  @moduledoc """
+  Documentation for `KeenAuthAzureAD`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KeenAuthAzureAD.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/keen_auth_azure_ad_test.exs
@@ -0,0 +1,8 @@
+defmodule KeenAuthAzureADTest do
+  use ExUnit.Case
+  doctest KeenAuthAzureAD
+
+  test "greets the world" do
+    assert KeenAuthAzureAD.hello() == :world
+  end
+end
